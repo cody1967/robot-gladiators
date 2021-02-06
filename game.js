02d0f2716feb4b883bed2ec6bcc738722c4a8665
Cody McNeill <cody@utahhydrographics.com>
2021-02-05 19:43:36 -0700

    new files

---
+++ game.js
@@ -0,0 +1,7 @@
+window.alert("this is an alert! JavaScript is running!");
+// this creates a function named "fight"
+function fight() {
+  window.alert ("the fight has begun!");
+}
+fight()
+window.prompt("what is your robot's name");
